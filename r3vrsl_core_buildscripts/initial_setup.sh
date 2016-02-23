8c5b3d723b735a80ba3a7551a9ac661cce542c51
InterGenOS <chris@intergenstudios.com>
2016-02-22 22:17:00 -0600

    Pushing Sources

--- r3vrsl_core_buildscripts/initial_setup.sh
+++ r3vrsl_core_buildscripts/initial_setup.sh
@@ -259,6 +259,33 @@ SETUP_BUILD () {
 
 }
 
+SETUP_CHROOT () {
+
+    # Set correct ownership
+    clear && HEADER1
+    echo -e "\n  ${GREEN}Changing temporary tools directory ownership...${NOCOLOR}\n\n"
+    chown -R root:root "$R3VRSL"/tools
+    sleep 2 && echo -e "\n\n  ${GREEN}Temporary tools directory ownership change complete${NOCOLOR}" && sleep 3
+
+    # Bind and mount system mounts
+    clear && HEADER1
+    echo -e "\n  ${GREEN}Preparing virtual kernel file system...${NOCOLOR}\n\n"
+    mkdir -pv "$R3VRSL"/{dev,proc,sys,run}
+    mknod -m 600 "$R3VRSL"/dev/console c 5 1
+    mknod -m 666 "$R3VRSL"/dev/null c 1 3
+    mount -v --bind /dev "$R3VRSL"/dev
+    mount -vt devpts devpts "$R3VRSL"/dev/pts -o gid=5,mode=620
+    mount -vt proc proc "$R3VRSL"/proc
+    mount -vt sysfs sysfs "$R3VRSL"/sys
+    mount -vt tmpfs tmpfs "$R3VRSL"/run
+    if [ -h "$R3VRSL"/dev/shm ]; then
+      mkdir -pv "$R3VRSL"/$(readlink "$R3VRSL"/dev/shm)
+    fi
+    sleep 2 && echo -e "\n\n  ${GREEN}Virtual kernel file system preparation complete${NOCOLOR}\n\n\n" && sleep 3
+    echo -e "  Entering chroot environment...\n\n" && sleep 3
+
+}
+
 
 ############################
 ##------------------------##
@@ -272,8 +299,139 @@ SETUP_BUILD () {
 ##---------------------##
 #########################
 
-HEADER1
-GET_TARGET_BUILD_PARTITION
+#------------------------#
+# Create log directories #
+#------------------------#
+
+mdkir -p /var/log/R3VRSL/BuildLogs/TempSys_Buildlogs || echo -e "\n\n  Unable to create log directories!\n\n  (exiting...)\n\n\n" > /home/failure
+if [ -f /home/failure ]; then
+    echo -e "\n\n  ${RED}FATAL ERROR${NOCOLOR}\n $(cat /home/failure)\n\n"
+    rm /home/failure
+    exit 1
+fi
+chmod -R 777 /var/log/R3VRSL
+
+#---------------------#
+# Begin initial setup #
+#---------------------#
+
+GET_TARGET_BUILD_PARTITION 2>&1 | tee /var/log/R3VRSL/BuildLogs/TempSys_Buildlogs/"$timestamp"_inital_setup.log
+sed -i -e 's/[\x01-\x1F\x7F]//g' -e 's|\[1m||g' -e 's|\[32m||g' -e 's|\[34m||g' -e 's|(B\[m||g' -e 's|\[1m\[32m||g' -e 's|\[H\[2J||g' -e 's|\[1m\[31m||g' -e 's|\[1m\[34m||g' -e 's|\[5A\[K||g' -e 's|\[1m\[33m||g' /var/log/R3VRSL/BuildLogs/TempSys_Buildlogs/"$timestamp"_inital_setup.log
+
+#------------------------------------------------------------------#
+# Begin temporary system build in separate shell as the build user #
+#------------------------------------------------------------------#
+
+cd "$R3VRSL" || echo -e "\n\n  Unable to move to build directory ${R3VRSL} for temp system build!\n\n  (exiting...)\n\n\n" > /home/failure
+if [ -f /home/failure ]; then
+    echo -e "\n\n  ${RED}FATAL ERROR${NOCOLOR}\n $(cat /home/failure)\n\n"
+    rm /home/failure
+    exit 1
+fi
+sudo -u r3vrsl ./clean_environment.sh &&
+printf "\n\n\n"
+
+#---------------------------------------------------------#
+# Setup the chroot environment for r3vrsl system building #
+#---------------------------------------------------------#
+
+SETUP_CHROOT 2>&1 | tee /var/log/R3VRSL/BuildLogs/"$timestamp"_setup_chroot.log
+sed -i -e 's/[\x01-\x1F\x7F]//g' -e 's|\[1m||g' -e 's|\[32m||g' -e 's|\[34m||g' -e 's|(B\[m||g' -e 's|\[1m\[32m||g' -e 's|\[H\[2J||g' -e 's|\[1m\[31m||g' -e 's|\[1m\[34m||g' -e 's|\[5A\[K||g' -e 's|\[1m\[33m||g' /var/log/R3VRSL/BuildLogs/"$timestamp"_setup_chroot.log
+
+#--------------------------------------------------#
+# Enter chroot to begin building the r3vrsl system #
+#--------------------------------------------------#
+
+cd "$R3VRSL" || echo -e "\n\n  Unable to move to build directory ${R3VRSL} for chroot entry!\n\n  (exiting...)\n\n\n" > /home/failure
+if [ -f /home/failure ]; then
+    echo -e "\n\n  ${RED}FATAL ERROR${NOCOLOR}\n $(cat /home/failure)\n\n"
+    rm /home/failure
+    exit 1
+fi
+sudo -u root ./enter_chroot.sh 2>&1 | tee /var/log/R3VRSL/BuildLogs/"$timestamp"_sys_build.log &&
+sed -i -e 's/[\x01-\x1F\x7F]//g' -e 's|\[1m||g' -e 's|\[32m||g' -e 's|\[34m||g' -e 's|(B\[m||g' -e 's|\[1m\[32m||g' -e 's|\[H\[2J||g' -e 's|\[1m\[31m||g' -e 's|\[1m\[34m||g' -e 's|\[5A\[K||g' -e 's|\[1m\[33m||g' /var/log/R3VRSL/BuildLogs/"$timestamp"_sys_build.log
+
+#----------------------------------------------------#
+# Re-enter chroot to begin using newly compiled bash #
+#----------------------------------------------------#
+
+clear && HEADER1
+echo -e "\n  ${GREEN}Entered Root Shell successfully${NOCOLOR}\n\n"
+DIVIDER1 BLUE && sleep 3
+echo -e "\n  ${GREEN}Entering chroot environment for post-bash package builds...${NOCOLOR}\n\n" && sleep 3
+cd "$R3VRSL" || echo -e "\n\n  Unable to move to build directory ${R3VRSL} for post-bash package builds!\n\n  (exiting...)\n\n\n" > /home/failure
+if [ -f /home/failure ]; then
+    echo -e "\n\n  ${RED}FATAL ERROR${NOCOLOR}\n $(cat /home/failure)\n\n"
+    rm /home/failure
+    exit 1
+fi
+sudo -u root ./enter_chroot_post-bash.sh 2>&1 | tee /var/log/InterGenOS/BuildLogs/"$timestamp"_chroot_post-bash.log &&
+sed -i -e 's/[\x01-\x1F\x7F]//g' -e 's|\[1m||g' -e 's|\[32m||g' -e 's|\[34m||g' -e 's|(B\[m||g' -e 's|\[1m\[32m||g' -e 's|\[H\[2J||g' -e 's|\[1m\[31m||g' -e 's|\[1m\[34m||g' -e 's|\[5A\[K||g' -e 's|\[1m\[33m||g' /var/log/R3VRSL/BuildLogs/"$timestamp"_chroot_post-bash.log
+printf "\n\n"
+
+#-------------------------------------------------------------#
+# Re-enter chroot to strip unnecessary binaries and libraries #
+#-------------------------------------------------------------#
+
+clear && HEADER1
+echo -e "\n  ${GREEN}Entered Root Shell successfully${NOCOLOR}\n\n"
+DIVIDER1 BLUE && sleep 3
+echo -e "\n  ${GREEN}Entering chroot environment for binary and library stripping...${NOCOLOR}\n\n" && sleep 3
+cd "$R3VRSL" || echo -e "\n\n  Unable to move to build directory ${R3VRSL} for binary and library stripping!\n\n  (exiting...)\n\n\n" > /home/failure
+if [ -f /home/failure ]; then
+    echo -e "\n\n  ${RED}FATAL ERROR${NOCOLOR}\n $(cat /home/failure)\n\n"
+    rm /home/failure
+    exit 1
+fi
+sudo -u root ./enter_chroot_stripping.sh 2>&1 | tee /var/log/InterGenOS/BuildLogs/"$timestamp"_chroot_stripping.log &&
+sed -i -e 's/[\x01-\x1F\x7F]//g' -e 's|\[1m||g' -e 's|\[32m||g' -e 's|\[34m||g' -e 's|(B\[m||g' -e 's|\[1m\[32m||g' -e 's|\[H\[2J||g' -e 's|\[1m\[31m||g' -e 's|\[1m\[34m||g' -e 's|\[5A\[K||g' -e 's|\[1m\[33m||g' /var/log/R3VRSL/BuildLogs/"$timestamp"_chroot_stripping.log
+printf "\n\n"
+
+#------------------------------------------#
+# Re-enter chroot to finalize system build #
+#------------------------------------------#
+
+clear && HEADER1
+echo -e "\n  ${GREEN}Entered Root Shell successfully${NOCOLOR}\n\n"
+DIVIDER1 BLUE && sleep 3
+echo -e "\n  ${GREEN}Entering chroot environment for System Finalization...${NOCOLOR}\n\n" && sleep 3
+cd "$R3VRSL" || echo -e "\n\n  Unable to move to build directory ${R3VRSL} for system finalization!\n\n  (exiting...)\n\n\n" > /home/failure
+if [ -f /home/failure ]; then
+    echo -e "\n\n  ${RED}FATAL ERROR${NOCOLOR}\n $(cat /home/failure)\n\n"
+    rm /home/failure
+    exit 1
+fi
+sudo -u root ./enter_chroot_finalize.sh 2>&1 | tee /var/log/InterGenOS/BuildLogs/"$timestamp"_chroot_finalize.log &&
+sed -i -e 's/[\x01-\x1F\x7F]//g' -e 's|\[1m||g' -e 's|\[32m||g' -e 's|\[34m||g' -e 's|(B\[m||g' -e 's|\[1m\[32m||g' -e 's|\[H\[2J||g' -e 's|\[1m\[31m||g' -e 's|\[1m\[34m||g' -e 's|\[5A\[K||g' -e 's|\[1m\[33m||g' /var/log/R3VRSL/BuildLogs/"$timestamp"_chroot_finalize.log
+printf "\n\n"
+
+#-----------------------#
+# Cleanup build scripts #
+#-----------------------#
+
+rm /build_* /clean_* /enter_* /finalize_* /strip_*
+
+#------------------------#
+# Completed Notification #
+#------------------------#
+
+clear && HEADER1
+echo -e "\n\n   ${GREY}R${RED}3${GREY}VRSL${NOCOLOR} ${WHITE}Behavioral.Code.Memory  Build:${NOCOLOR}1.0 ${GREEN}COMPLETED${NOCOLOR}\n\n" && sleep 3
+DIVIDER1 BLUE
+echo -e "  ${WHITE}Thank you for your participation in the project!${NOCOLOR}\n\n"
+echo -e "  ${WHITE}Any errors you find and would like to report will be greatly appreciated!${NOCOLOR}\n\n"
+echo -e "  ${WHITE}Send any submissions to:${NOCOLOR}\n\n\n"
+echo -e "                         ${GREEN}R3VRSL${WHITE}@${GREEN}R3VRSL.COM${NOCOLOR}\n\n\n"
+echo -e "  ${RED}${BLINK}***${NOCOLOR}${YELLOW}IMPORTANT - ${RED}${UNDERLINE}IMPORTANT${NOCOLOR}${YELLOW} - IMPORTANT${RED}${BLINK}***${NOCOLOR}\n\n"
+echo -e "  ${WHITE}            YOU MUST NOW DO THE FOLLOWING:${NOCOLOR}\n"
+echo -e "                      ${GREEN}1)${WHITE} Re-Install Grub (or your preferred bootloader)${NOCOLOR}\n"
+echo -e "                             ie:  'sudo grub-install /dev/sd_'"
+echo -e "                      ${GREEN}2)${WHITE} Create a new bootloader config${NOCOLOR}\n"
+echo -e "                             ie:  'sudo grub-mkconfig -o /boot/grub/grub.cfg'\n\n"
+echo -e "  ${RED}${BLINK}***${NOCOLOR}${YELLOW}IMPORTANT - ${RED}${UNDERLINE}IMPORTANT${NOCOLOR}${YELLOW} - IMPORTANT${RED}${BLINK}***${NOCOLOR}\n\n"
+echo -e "  ${WHITE}You should then be able to restart and select 'R3VRSL' from your boot menu"
+DIVIDER1 BLUE && sleep 3
+
 
 #######################
 ##-------------------##
